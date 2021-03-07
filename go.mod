e1bf0ded821dfb7b65af11612c9082cc899118c2
DragonTaro1031 <dragon.taro.lioil@gmail.com>
2021-03-07 17:06:16 +0900

    test-executorのhcを叩くコードを書いた

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Levetty/bitrise-step-smartqa
+
+go 1.15
+
+require github.com/go-resty/resty/v2 v2.5.0 // indirect
--- main.go
+++ main.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-resty/resty/v2"
 	"os"
-	"os/exec"
 )
 
 func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
-
 	//
 	// --- Step Outputs: Export Environment Variables for other Steps:
 	// You can export Environment Variables for other Steps with
 	//  envman, which is automatically installed by `bitrise setup`.
 	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+
+	client := resty.New()
+
+	resp, err := client.R().
+		EnableTrace().
+		Get("https://test-executor.smart-qa.io/")
+
 	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
+		fmt.Println(err)
 	}
+
+	fmt.Println(resp)
+
+	//cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	//if err != nil {
+	//	fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+	//	os.Exit(1)
+	//}
+
 	// You can find more usage examples on envman's GitHub page
 	//  at: https://github.com/bitrise-io/envman
 
