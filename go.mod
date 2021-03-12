4e94166633379d698c6618b154f82a0bd69467d0
DragonTaro1031 <dragon.taro.lioil@gmail.com>
2021-03-12 21:57:01 +0900

    入力とか依存ライブラリとかを動くようにした

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Levetty/bitrise-step-smartqa
 
 go 1.15
+
+require (
+	github.com/bitrise-io/go-utils v0.0.0-20201211082830-859032e9adf0 // indirect
+	github.com/bitrise-tools/go-steputils v0.0.0-20201016102104-03ae3a6ded35
+	github.com/go-resty/resty/v2 v2.5.0
+)
--- main.go
+++ main.go
@@ -2,40 +2,32 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
+	"github.com/bitrise-tools/go-steputils/stepconf"
+	"github.com/go-resty/resty/v2"
 )
 
+type Config struct {
+	BuildPath          string          `env:"build_path,required"`
+	APIKey             stepconf.Secret `env:"api_key,required"`
+}
+
 func main() {
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
+	var cfg Config
+	if err := stepconf.Parse(&cfg); err != nil {
+		fmt.Println(err.Error())
+	}
 
-	url := "https://test-executor.smart-qa.io/"
+	client := resty.New()
 
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
+	resp, err := client.R().
+		EnableTrace().
+		Get("https://test-executor.smart-qa.io/")
 
-	byteArray, _ := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	fmt.Println(string(byteArray))
-
-	//cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
-	//if err != nil {
-	//	fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-	//	os.Exit(1)
-	//}
-
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
-
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	fmt.Println(resp)
 	os.Exit(0)
 }
